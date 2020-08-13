d553284e62746d2bfffc32724dfbe5420fd65ac2
AJ Clemente <areljann.c@gmail.com>
2020-08-13 18:02:57 -0400

    MISC: Remove `create-react-app` boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Component
+const App = () => (
+  <div
+    style={{
+      width: `100vw`,
+      height: `100vh`,
+      display: `flex`,
+      justifyContent: `center`,
+      alignItems: `center`
+    }}
+  >
+    <p>test</p>
+  </div>
+)
 
-export default App;
+export default App
